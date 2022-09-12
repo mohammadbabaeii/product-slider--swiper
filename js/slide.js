074d8c21d0113c5694e9b532118e037c4b27307c
mohammad <mohammadbabaei.821@gmail.com>
2022-09-12 15:46:25 +0430

    swiper slider

---
+++ js/slide.js
@@ -0,0 +1,13 @@
+
+
+// swiper
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
